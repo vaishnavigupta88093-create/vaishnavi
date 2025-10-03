c0ff28fb0eae17e808f7f8eef78bbc94cad1f598
vaishnavi-codes <vaishnavigupta88093@gmail.com>
2025-10-03 14:54:10 +0530

    Add files via upload

---
+++ vaishanvi.c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int age;
+printf("enter your age\n");
+scanf("%d",&age);
+
+if(age>=18){
+    printf("ready for voting\n");
+}
+else{
+    printf("not eligible\n");
+}
+
+
+
+
+
+
+
+
+
+
+    return 0;
+} 
